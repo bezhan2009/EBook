ce9707604ad159442d0c4c0979e084ad34200089
AndreyAko57 <andrey57@mail.ru>
2024-01-31 14:45:37 +0500

    [refactor] Modify All files...

--- connection.py
+++ connection.py
@@ -1,8 +1,14 @@
-from sqlalchemy import create_engine
 from security import dbname_app, user_app, password_app, host_app, port_app
 
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
 # db_url = 'postgresql://username:password@host:port/dbname'
-db_url = f'postgresql://{user_app}:{password_app}@{host_app}:{port_app}/{dbname_app}'
+db_url = f'postgresql://{user_app}:{password_app}@{
+    host_app}:{port_app}/{dbname_app}'
 
 # создание машины соединения
 engine = create_engine(db_url, echo=False)
+
+# Создаем класс последующих сессий, на основе которого будут создаваться разовые экземпляры для разовых подключений.
+Session = sessionmaker(bind=engine)
