40c1e755626754b3dde6953e1c7600de84b3dd8e
AndreyAko57 <andrey57@mail.ru>
2024-01-31 10:20:08 +0500

    [refactor] Modify models file...

--- models.py
+++ models.py
@@ -16,13 +16,15 @@ class Books(Base):
                 nullable=False, index=True)
     title = Column(String(length=255), nullable=False, unique=False)
     publication = Column(String(length=50), nullable=False, unique=False)
-    publication_date = Column(String(length=50), nullable=False, unique=False)
-    # number_of_copies = Column(Integer, nullable=False, unique=False)
+    publication_date = Column(DateTime, nullable=False)
     cover_image = Column(String)
     # floor, row, rack, shelf
-    book_location = Column(String(length=8), nullable=False)
+    book_location = Column(String(length=8), nullable=False,
+                           default='00000000', server_default=text('00000000'))
     other_attribute = Column(String(length=120))
     price = Column(Float, nullable=False)
+    available_copies = Column(Integer, nullable=False,
+                              default=0, server_default=text('0'))
 
 
 class BooksAuthors(Base):
@@ -100,50 +102,19 @@ class OrderItems(Base):
 
     id = Column(Integer, primary_key=True)
     order_id = Column(Integer, ForeignKey('orders.id'), nullable=False)
-    new_book_id = Column(Integer, ForeignKey('books.id'), nullable=True)
-    new_book_title = Column(String, nullable=False)
-    new_book_author = Column(String, nullable=False)
+    new_book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
     new_book_price = Column(Float, nullable=False)
     quantity = Column(Integer, nullable=False)
+    new_book = relationship("Books", foreign_keys=[new_book_id])
 
 
-# ########################################################################### #
-class Users(Base):
-    __tablename__ = "users"
+class Staff(Base):
+    __tablename__ = "staff"
 
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, index=True)
-    login = Column(String(length=32), nullable=False, unique=True)
-    psw = Column(String(length=255), nullable=False)
-    access_level = Column(SmallInteger, default=1, server_default=text('1'))
-
-
-class Customers(Base):
-    __tablename__ = "customers"
-
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                nullable=False, index=True)
-    name_cust = Column(String(length=32), nullable=False, unique=True)
-    is_deleted = Column(Boolean, nullable=False,
-                        default=False, server_default=text('False'))
-
-    accounts = relationship("Accounts", back_populates="customer")
-
-
-class Accounts(Base):
-    __tablename__ = "accounts"
-
-    id = Column(Integer, primary_key=True, autoincrement=True,
-                index=True, nullable=False)
-    cust_id = Column(Integer, ForeignKey('customers.id'), nullable=False)
-    account_number = Column(String(length=16), nullable=False, unique=True)
-    is_deleted = Column(Boolean, nullable=False,
-                        default=False, server_default=text('False'))
-    date_deleted = Column(DateTime, nullable=True)
-    amount = Column(Numeric(precision=10, scale=2),
-                    nullable=False, default=10000, server_default=text('10000'))
-
-    customer = relationship("Customers", back_populates="accounts")
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(length=70), nullable=False)
+    role = Column(String(length=32), nullable=False)
+    access_level = Column(Integer, nullable=False)
 
 
 Base.metadata.create_all(bind=engine)
