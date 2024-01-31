c5de7d391141815c883b01939aa0ec4699ed17cb
Nikolai-Nikolin <nikolay.nikolin@gmail.com>
2024-01-31 13:43:08 +0500

    [refactor] fix connections

--- security.py
+++ security.py
@@ -2,8 +2,8 @@
 
 # for connect db
 
-dbname_app = "eovqbhzz"
-user_app = "eovqbhzz"
+dbname_app = "ywijipwx"
+user_app = "ywijipwx"
 password_app = "kLQiABwQ5bMg5J_MSk1Mh99YVMw7jdMd"
 host_app = "tiny.db.elephantsql.com"
 port_app = "5432"
